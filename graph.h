680c90ad138ad8480f39172254fd295e1c2ff7d2
Meka Sai Mukund <mukundmeka3456@gmail.com>
2021-04-26 17:17:59 +0530

    Included the 2 HashTable structs

--- graph.h
+++ graph.h
@@ -1,23 +1,31 @@
 #pragma once
 
-#define WHITE  1        // visited vertices
-#define GRAY   0        // about to be visited
-#define BLACK -1        // not visited
-#define INFTY  100000000
-#define UNKNOWN -1  
+#define WHITE 1  // visited vertices
+#define GRAY 0   // about to be visited
+#define BLACK -1 // not visited
+#define INFTY 100000000
+#define UNKNOWN -1
 #define NOTVERTEX -1
 
 typedef struct TradeBank *PtrToGraph;
-typedef struct Currency *PtrToNext;
+typedef struct CurrIn *PtrToNext;
+typedef struct CurrOut *PtrToNext;
 typedef int ElemType;
 
 struct TradeBank
 {
-    int NumVertex;      // Number of vertices in the graph
+    int NumVertex;       // Number of vertices in the graph
     PtrToNext *GraphPtr; // Ptr to each graph (tradebank).
 };
 
-struct Currency
+struct CurrIn
+{
+    ElemType VertexID; // Name of the vertex
+    ElemType ConvRate; // Edge weight
+    PtrToNext Next;    // Pointer to Next Node
+};
+
+struct CurrOut
 {
     ElemType VertexID; // Name of the vertex
     ElemType ConvRate; // Edge weight
@@ -26,6 +34,10 @@ struct Currency
 
 struct AllGraph
 {
-    int NumBanks;           // number of trade banks present
-    PtrToGraph *GraphPtr;   // pointer to list of trade banks
+    int NumBanks;         // number of trade banks present
+    PtrToGraph *GraphPtr; // pointer to list of trade banks
 };
+
+// Let's say A->B (A nad B are vertices and -> is a directed edge) is present in the graph
+// Then, in CurrOut HashTable(adjacency list), the neighbor of B will be A
+// Similarly, in CurrIn HashTable(adjacency list), neighbor of A will be B  
