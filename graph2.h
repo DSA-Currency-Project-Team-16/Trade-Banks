4af2ec8da05c92650b41ca66ddb0d108801eab84
Srija <83160410+srija-13@users.noreply.github.com>
2021-05-08 11:42:57 +0530

    Add files via upload

---
+++ graph2.h
@@ -0,0 +1,42 @@
+#ifndef _GRAPH_
+#define _GRAPH_
+
+#define tablesize 20
+
+typedef char ElemType;
+typedef struct TradeBank *PtrToGraph;
+typedef struct Node *PtrToNext;
+
+struct TradeBank {
+    ElemType TradeBank[50];
+    int numVertex;
+    PtrToNext GraphIn;
+    PtrToNext GraphOut;
+    PtrToGraph Next;
+};
+typedef struct TradeBank TradeBank;
+
+struct Node {
+    ElemType VertexID[50];
+    int ConvRate;
+    PtrToNext Next;
+};
+typedef struct Node Node;
+
+struct AllGraph {
+    int NumBanks;
+    PtrToGraph GraphPtr;
+}; 
+typedef struct AllGraph AllGraph;
+
+PtrToGraph CreateGraph(int numVertex, ElemType* TradeBank);
+PtrToNext CreateNode(ElemType VertexID[50]);
+
+void InsertNode(ElemType TradeBank[50], ElemType VertexID[50], AllGraph* list);
+
+void InsertEdge(ElemType TradeBank[50], ElemType C1[50], ElemType C2[50], int ConvRate, AllGraph* list);
+
+void PrintGraph(AllGraph list);
+void AddGraph(ElemType TradeBank[50], int numVertex, AllGraph* list);
+
+#endif
